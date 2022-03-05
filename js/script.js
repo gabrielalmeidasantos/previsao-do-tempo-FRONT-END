e193fc0f5df63e90affe0c22e32c7bf3ac2ef373
gabrielalmeidasantos <gabriel.almeiidda@gmail.com>
2022-03-05 16:52:59 -0300

    front end terminado, mas esta sem back end porque nao encontrei nenhuma API adequada que funcionasse

--- js/script.js
+++ js/script.js
@@ -7,28 +7,4 @@ const fechar = () => {
 const buscar = (e) => {
     e.preventDefault();
     card.style.display = "block";
-
-    getApi("campinas")
-        .then((json) => {
-            console.log(json)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-};
-
-const getApi = (nome) => {
-    return new Promise((resolve, reject) => {
-        // fetch(`https://api.github.com/users/${nome}`)
-        fetch(`http://api.hgbrasil.com/weather?key=05bf96d7&city_name=${nome}`)
-            .then((response) => {
-                if (response.status == 404) {
-                    reject(response.message);
-                }
-                resolve(response.json());
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
 };
